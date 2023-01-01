66d8d3f7960fc759be3fe8cf4740ce67546d4715
Pratik <pratikanil@techis.io>
2023-01-01 13:49:17 +0530

    Updated backend code for replit

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://frontend-electric-views.herokuapp.com']
+X_FRAME_OPTIONS = '*'
+CORS_ORIGIN_ALLOW_ALL = True
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000/']
 
 # Application definition
 
